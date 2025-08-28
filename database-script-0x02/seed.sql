44db07846575c8008c85c6a3eb1615e796479faf
James Appiah <159532433+qwabena37@users.noreply.github.com>
2025-08-28 00:08:32 +0000

    Update seed.sql

--- database-script-0x02/seed.sql
+++ database-script-0x02/seed.sql
@@ -1,15 +1,15 @@
 -- USERS
 INSERT INTO User (user_id, first_name, last_name, email, password_hash, phone_number, role)
 VALUES
-  ('uuid-001', 'Ama', 'Mensah', 'ama.mensah@example.com', 'hashed_pw_ama', '233201234567', 'host'),
-  ('uuid-002', 'Kwame', 'Boateng', 'kwame.boateng@example.com', 'hashed_pw_kwame', '233209876543', 'guest'),
-  ('uuid-003', 'Linda', 'Owusu', 'linda.owusu@example.com', 'hashed_pw_linda', NULL, 'guest');
+  ('uuid-001', 'Eric', 'Owusu', 'eric.owusu@example.com', 'hashed_pw_ama', '233201234567', 'host'),
+  ('uuid-002', 'Joe', 'Wallace', 'joe.wallace@example.com', 'hashed_pw_kwame', '233209876543', 'guest'),
+  ('uuid-003', 'Nadine', 'Asomani', 'nadine.asomani@example.com', 'hashed_pw_linda', NULL, 'guest');
 
 -- PROPERTIES
 INSERT INTO Property (property_id, host_id, name, description, location, pricepernight)
 VALUES
-  ('prop-001', 'uuid-001', 'Cozy Beachfront Chalet', 'A serene escape by the coast of Ada.', 'Ada, Ghana', 350.00),
-  ('prop-002', 'uuid-001', 'Modern Accra Apartment', 'Stylish 2-bedroom flat near Osu.', 'Accra, Ghana', 500.00);
+  ('prop-001', 'uuid-001', 'Luxurious Airport View', 'A serene escape by the East of Airport.', 'Accra, Ghana', 680.00),
+  ('prop-002', 'uuid-001', 'Classic Home Apartment', 'Stylish 4-bedroom aparatment near Shiashie.', 'Accra, Ghana', 800.00);
 
 -- BOOKINGS
 INSERT INTO Booking (booking_id, property_id, user_id, start_date, end_date, status)
@@ -26,11 +26,11 @@ VALUES
 -- REVIEWS
 INSERT INTO Review (review_id, property_id, user_id, rating, comment)
 VALUES
-  ('rev-001', 'prop-001', 'uuid-002', 5, 'Absolutely loved the beachfront view!'),
-  ('rev-002', 'prop-002', 'uuid-003', 4, 'Great location, but Wi-Fi was spotty.');
+  ('rev-001', 'prop-001', 'uuid-002', 5, 'Greatly loved the airport view!'),
+  ('rev-002', 'prop-002', 'uuid-003', 4, 'Amazing location, but it was quite noisy.');
 
 -- MESSAGES
 INSERT INTO Message (message_id, sender_id, recipient_id, message_body)
 VALUES
-  ('msg-001', 'uuid-002', 'uuid-001', 'Hi Ama, is the beachfront chalet available next weekend?'),
-  ('msg-002', 'uuid-001', 'uuid-002', 'Yes Kwame, it’s available. I’ll block the dates for you.');
+  ('msg-001', 'uuid-002', 'uuid-001', 'Hello Ben, is the Luxurious Airport View available next weekend?'),
+  ('msg-002', 'uuid-001', 'uuid-002', 'Yes Eric, it’s available. I’ll block the dates for you.');
